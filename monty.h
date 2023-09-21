dce19b8b6916d892b6015e3c9d12ac550756c79e
Omar <omararaby2001@gmail.com>
2023-09-21 15:06:33 -0700

    add all prototypes

--- monty.h
+++ monty.h
@@ -41,12 +41,23 @@ typedef struct stack_s
 typedef struct instruction_s
 {
 	char *opcode;
-	void (*f)(stack_t **stack, int val);
+	void (*f)(stack_t **stack, int val, unsigned int line_num);
 } instruction_t;
 
 int main(int argc, char **argv);
-void _push(stack_t **head, int val);
-void _pall(stack_t **head, int val);
-void _pint(stack_t **head, int val);
-void _pop(stack_t **head, int val);
+void _push(stack_t **head, int val, unsigned int line_num);
+void _pall(stack_t **head, int val, unsigned int line_num);
+void _pint(stack_t **head, int val, unsigned int line_num);
+void _pop(stack_t **head, int val, unsigned int line_num);
+void _swap(stack_t **head, int val, unsigned int line_num);
+void _add(stack_t **head, int val, unsigned int line_num);
+void _nop(stack_t **head, int val, unsigned int line_num);
+void _sub(stack_t **head, int val, unsigned int line_num);
+void _div(stack_t **head, int val, unsigned int line_num);
+void _mul(stack_t **head, int val, unsigned int line_num);
+void _mod(stack_t **head, int val, unsigned int line_num);
+void _pchar(stack_t **head, int val, unsigned int line_num);
+void _rotl(stack_t **head, int val, unsigned int line_num);
+void _rotr(stack_t **head, int val, unsigned int line_num);
+
 #endif
