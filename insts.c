084edef980e0c2ff3a690275028f430caceb97a2
Omar <omararaby2001@gmail.com>
2023-09-21 15:04:39 -0700

    push pall pint pop nop

--- insts.c
+++ insts.c
@@ -3,11 +3,13 @@
  * _push - pushes an element to the stack
  * @head: pointer to pointer to stack_t
  * @val: value to push
+ * @line_num: unused
  * Return: nothing
  */
-void _push(stack_t **head, int val)
+void _push(stack_t **head, int val, unsigned int line_num)
 {
 	stack_t *new_node;
+	(void)line_num;
 
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
@@ -27,13 +29,15 @@ void _push(stack_t **head, int val)
 /**
  * _pall - prints all the elements of the stack
  * @head: pointer to pointer to stack_t
- * @val: unused here
+ * @val: unused
+ * @line_num: number of lines
  * Return: nothing
  */
-void _pall(stack_t **head, int val)
+void _pall(stack_t **head, int val, unsigned int line_num)
 {
 	stack_t *temp = *head;
 	(void)val;
+	(void)line_num;
 
 	if (head == NULL)
 	{
@@ -48,17 +52,18 @@ void _pall(stack_t **head, int val)
 /**
  * _pint - prints the value at the top
  * @head: head of the stack
- * @val: number of lines
+ * @val: unused
+ * @line_num: number of lines
  * Return: nothing
  */
 
-void _pint(stack_t **head, int val)
+void _pint(stack_t **head, int val, unsigned int line_num)
 {
 	(void)val;
 
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%u: can't pint, stack empty\n", val);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*head)->n);
@@ -66,21 +71,35 @@ void _pint(stack_t **head, int val)
 /**
  * _pop - removes the top element of the stack
  * @head: head of the stack
- * @val: number of lines
+ * @val: unused
+ * @line_num: number of lines
  * Return: nothing
  */
 
-void _pop(stack_t **head, int val)
+void _pop(stack_t **head, int val, unsigned int line_num)
 {
 	stack_t *other;
 	(void)val;
 
 	if (*head == NULL || head == NULL)
 	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", val);
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 	other = *head;
 	*head = other->next;
 	free(other);
+}
+/**
+ * _nop- nothing
+ * @head: stack head
+ * @val: unused
+ * @line_num: number of lines
+ * Return: no return
+ */
+void _nop(stack_t **head, int val, unsigned int line_num)
+{
+	(void)line_num;
+	(void)head;
+	(void)val;
 }
